48d32fd3721136d5756f298936353db23c8b52d7
Hero <gzoo@uzwelo.com>
2021-06-21 21:10:29 -0400

    Testing

---
+++ src/runtime.ts
@@ -0,0 +1,21 @@
+export const userSelections: Object = {
+    weapon1: { weaponDropdown1: "", weaponSize1: "" },
+    weapon2: { weaponDropdown2: "", weaponSize2: "" },
+    weapon3: { weaponDropdown3: "", weaponSize3: "" },
+    weapon4: { weaponDropdown4: "", weaponSize4: "" },
+    distro: {
+        distroSizeDropdown: "",
+        distroClassDropdown: "",
+        distroEngineeringDropdown: "",
+        distroExperimentalDropdown: ""
+    },
+    targetSelectionDropdown: "",
+    ammoSelectionDropdown: "",
+    customAccuracy: ""
+};
+
+export const userInputs: Object = {
+    sinks: "",
+    range: "",
+    shipName: ""
+};
